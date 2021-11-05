2472971a39ba7aa04f2e5bb7bfe85938f4338535
Wladimir Ribeiro de Souza <wladimirsouza@outlook.com>
2021-11-05 11:51:31 -0300

    Criado a parte para receber notificações.

--- LumberjackRockClub/LumberjackRockClub.Android/MainActivity.cs
+++ LumberjackRockClub/LumberjackRockClub.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Plugin.FirebasePushNotification;
 
 namespace LumberjackRockClub.Droid
 {
@@ -18,6 +19,8 @@ namespace LumberjackRockClub.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            FirebasePushNotificationManager.ProcessIntent(this, Intent);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
--- LumberjackRockClub/LumberjackRockClub/App.xaml.cs
+++ LumberjackRockClub/LumberjackRockClub/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.FirebasePushNotification;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,15 @@ namespace LumberjackRockClub
             InitializeComponent();
 
             MainPage = new NavigationPage ( new View.MainView() );
-            
+
+            CrossFirebasePushNotification.Current.OnTokenRefresh += Current_OnTokenRefresh;
+
+        }
+
+        private void Current_OnTokenRefresh(object source, FirebasePushNotificationTokenEventArgs e)
+        {
+            //throw new NotImplementedException();
+            System.Diagnostics.Debug.WriteLine($"Token: {e.Token}");
         }
 
         protected override void OnStart()
